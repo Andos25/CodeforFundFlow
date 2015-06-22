8a02a7efae6e36834679c892ee2a1ea4485fd255
kangkona <kangkona@gmail.com>
2015-06-22 18:08:32 +0800

    newest

--- kangkona/code/record.py
+++ kangkona/code/record.py
@@ -13,7 +13,7 @@ std of errors:  0.158459638824
 next step:  在计算净流入的情况下考虑利率波动
 净流入 = (流入 - 收益) - (流出 - 购物)
 '''
-
+#      n_estimators,max_depth,learning_rate
 '''
 115: purchase:{639,1,0.01}       redeem{665,3,0.02},  特征是new文件夹下的特征
 '''
